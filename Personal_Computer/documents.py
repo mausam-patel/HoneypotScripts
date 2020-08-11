8fe8d3693e864e44b2df14e46e9ec064fcba45a2
mausam-patel <56285380+mausam-patel@users.noreply.github.com>
2020-08-11 19:58:24 -0400

    Create documents.py

---
+++ Personal_Computer/documents.py
@@ -0,0 +1,6 @@
+import subprocess 
+from faker import Faker 
+fake = Faker()
+for x in range():
+        fakeFile = ' ' + fake.word(ext_word_list=None) + “.docx” 
+   subprocess.call(["touch", fakeFile])
